fe3dfc62efa66ac2cf1d422eb8670a20e4435356
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-02-23 14:21:00 +0530

    url params

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom"
+
 export default function UserDetail() {
+  //useParams() hook returns an object with key value pairs, this object contains key value pairs of the dynamic params from the current url. 
+  const params = useParams()
+
+  //It is important to note here that userId on the params object correponds to the dynamic segment we have specified in the route config :userId
+  const userId = params.userId
+
+  //you can also destructure it
+  // const {useId} = useParams()
   return (
-    <div>Details of the user</div>
+    <div>Details of the user {userId}</div>
   )
 }
